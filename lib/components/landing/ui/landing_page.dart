39db1e36e5fad3404263745e9474522f9b16bc6f
Jbinoggi <biernackijan4@gmail.com>
2021-10-04 19:21:49 +0200

    #11 Flutter gen

--- lib/components/landing/ui/landing_page.dart
+++ lib/components/landing/ui/landing_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:zample/components/landing/bloc/cubit/social_links_cubit.dart';
 import 'package:zample/components/landing/ui/widgets/social_links.dart';
+import 'package:zample/gen/assets.gen.dart';
 
 /// Die LandingPage dient zur Authentifizierung
 /// des Users.
@@ -16,10 +17,10 @@ class LandingPage extends StatelessWidget {
         Container(
           height: size.height,
           width: size.width,
-          decoration: const BoxDecoration(
+          decoration: BoxDecoration(
             image: DecorationImage(
-              image: AssetImage("assets/images/background.jpeg"),
-              fit: BoxFit.cover,
+              image: AssetImage(Assets.images.background.path),
+              fit: BoxFit.fill,
             ),
           ),
         ),
@@ -36,19 +37,15 @@ class LandingPage extends StatelessWidget {
                     child: Column(
                       children: [
                         Container(
-                          padding: EdgeInsets.only(
-                            left: size.width * 0.1,
-                            right: size.width * 0.1,
-                            top: size.height * 0.15,
-                            bottom: size.height * 0.02,
-                          ),
-                          alignment: Alignment.topCenter,
-                          child: Image.asset(
-                            "assets/images/zample.png",
-                            height: 120,
-                            width: 120,
-                          ),
-                        ),
+                            padding: EdgeInsets.only(
+                              left: size.width * 0.1,
+                              right: size.width * 0.1,
+                              top: size.height * 0.15,
+                              bottom: size.height * 0.02,
+                            ),
+                            alignment: Alignment.topCenter,
+                            child: Assets.images.zample
+                                .image(height: 120, width: 120)),
                         Text(
                           "Welcome to Zample",
                           style: Theme.of(context).textTheme.headline2,
